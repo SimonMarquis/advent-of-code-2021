6177694059eba2589ec6eba8e46b153198f52d66
Simon Marquis <contact@simon-marquis.fr>
2022-01-04 19:09:04 +0100

    Day 09

---
+++ src/main/kotlin/Day09.kt
@@ -0,0 +1,54 @@
+import kotlin.math.abs
+
+class Day09(raw: List<String>) {
+
+    private val input = raw.mapIndexed { y, line ->
+        line.mapIndexed { x, height ->
+            Point(x, y, height.toString().toInt())
+        }.toTypedArray()
+    }.toTypedArray().let(::HeightMap)
+
+    fun part1(): Int = with(input) {
+        points.filter { it.isLow() }
+    }.sumOf { it.riskLevel }
+
+    fun part2(): Int = with(input) {
+        points.groupingBy { it.flood() }.eachCount()
+    }.filterKeys { it != null }
+        .entries.sortedByDescending { it.value }
+        .take(3)
+        .fold(1) { acc, next -> acc * next.value }
+
+    class HeightMap(private val map: Array<Array<Point>>) {
+
+        val points: List<Point> get() = map.flatten()
+
+        operator fun get(x: Int, y: Int): Point = map[y][x]
+
+        private fun Point.neighbours(): Set<Point> = buildSet {
+            (-1..1).forEach { dy ->
+                (-1..1).forEach { dx ->
+                    when {
+                        abs(dx) == abs(dy) -> Unit
+                        else -> map.getOrNull(y + dy)?.getOrNull(x + dx)?.let(::add)
+                    }
+                }
+            }
+        }
+
+        fun Point.isLow() = neighbours().all { height < it.height }
+
+        fun Point.flood(seen: Set<Point> = setOf(this)): Point? {
+            if (height == 9) return null
+            if (isLow()) return this
+            val candidates = neighbours().filter { it.height < height } - seen
+            return candidates.firstNotNullOfOrNull { it.flood(seen + candidates) }
+        }
+
+    }
+
+    data class Point(val x: Int, val y: Int, val height: Int) {
+        val riskLevel = height.inc()
+    }
+
+}
